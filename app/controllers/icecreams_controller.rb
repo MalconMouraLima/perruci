9f62419e0ef6d836b8cb84fe6a3d9a4e429c0476
MalconMouraLima <malcon.moura.lima@gmail.com>
2017-04-30 02:34:47 -0300

    20170430_0234

--- app/controllers/icecreams_controller.rb
+++ app/controllers/icecreams_controller.rb
@@ -1,17 +1,37 @@
 class IcecreamsController < ApplicationController
+  before_action :find_icecream, only: [:show, :edit, :updade, :destroy]
 
   def index
+		@icecream = Icecream.all.order("created_at DESC")
+	end
+
+  def show
   end
 
   def new
+    @icecream = Icecream.new
   end
 
   def create
-  end
+    @icecream = Icecream.new(icecream_params)
 
+    if @icecream.save
+      redirect_to @icecream, notice: "Sorvete salvo com sucesso!"
+    else
+      render 'new'
+    end
+
+  end
 
   private
 
-  def icecream_params    
+  def icecream_params
+    params.require(:icecream).permit(:title, :description)
   end
+
+  def find_icecream
+    @icecream = Icecream.find(params[:id])
+  end
+
+
 end
